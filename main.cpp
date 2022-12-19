c3f7790b55a87c3d8ae2f42d2b9599ab86ef4c36
Andrea-Fiori <115191307+Andrea-Fiori@users.noreply.github.com>
2022-12-19 21:08:28 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,11 +3,11 @@
 
 using namespace std;
 
-void principale();
-void bisezione(float, float );
 float f(float a);
+bool control(float a, float b);
 float rid(float a);
-bool control(float, float);
+void bisezione(float a, float b);
+void principale();
 
 int main() {
     principale();
@@ -22,49 +22,17 @@ void principale(){
     cin >> b;
     bisezione(a,b);
 }
+
 float f(float a){
-    float b;
+    float b = 0;
     b = (pow(a,2) * cos(a) + 1);
     return b;
 }
+
 float rid(float a){
     return floorf(a * 10000) / 10000;
 }
-void bisezione(float a, float b){
-    float x = 0 ;
-    float error = 0;
-    if (control(a,b)){
-    do{
-            x = (a+b)/2;
-            if (f(x) == 0)
-            {
-                break;
-            }
-            else if (f(x )*f(b) < 0 )
-            {
-                a = x;
-                error = abs( (b-a)/2 );
-            }
-            else
-            {
-                b = x;
-                error = abs( (b-a)/2 );
 
-
-            }
-    } while (error >= 1/ pow(M_E, 6));
-    cout << rid(x)<< endl;
-    cout << rid(f(x));
-    }
-    else {
-        do {
-        cout << "inserire estremi" << endl;
-        cin >> a;
-        cin >> b;
-        } while (control(a,b) == false);
-        bisezione(a,b);
-    }
-}
 bool control(float a, float b){
     if (f(a)*f(b)< 0)
     {
@@ -73,5 +41,40 @@ bool control(float a, float b){
     else
     {
         return false;
+    }
+}
+
+void bisezione(float a, float b){
+    float x = 0 ;
+    float error = 0;
+    if (control(a,b))
+    {
+        do{
+            x = (a + b) / 2;
+            if (f(x) == 0)
+            {
+                break;
+            }
+            else if (f(x) * f(b) < 0 )
+            {
+                a = x;
+                error = abs( (b - a) / 2 );
+            }
+            else
+            {
+                b = x;
+                error = abs( (b - a ) / 2 );
+            }
+        } while (error >= 1/ pow(M_E, 6));
+        cout << rid(x)<< endl;
+        cout << rid(f(x));
+    }
+    else {
+        do {
+            cout << "inserire estremi" << endl;
+            cin >> a;
+            cin >> b;
+        } while (!control(a, b));
+        bisezione(a,b);
     }
 }
