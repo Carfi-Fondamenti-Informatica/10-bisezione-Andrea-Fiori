52470d4d16c015d7305ba2858365d513d2454d8b
Andrea-Fiori <115191307+Andrea-Fiori@users.noreply.github.com>
2022-12-19 22:31:52 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,36 +3,65 @@
 
 using namespace std;
 
-float f(float a);
-bool control(float a, float b);
-float rid(float a);
+void principale(float a, float b);
 void bisezione(float a, float b);
-void principale();
+float f(float a);
+float rid(float a);
+bool control(float a, float b);
+void imput(float a, float b);
 
 int main() {
-    principale();
+    float a = 0;
+    float b = 0;
+    principale(a,b);
     return 0;
 }
 
-void principale(){
-    cout << "inserire estremi" << endl;
-    float a = 0;
-    float b = 0;
-    cin >> a;
-    cin >> b;
+void principale(float a, float b){
+    imput(a,b);
     bisezione(a,b);
 }
-
 float f(float a){
-    float b = 0;
+    float b;
     b = (pow(a,2) * cos(a) + 1);
     return b;
 }
-
 float rid(float a){
     return floorf(a * 10000) / 10000;
 }
+void bisezione(float a, float b){
+    float x = 0 ;
+    float error = 0;
+    if (control(a,b))
+    {
+        do{
+            x = (a+b)/2;
+            if (f(x) == 0)
+            {
+                break;
+            }
+            else if (f(x )*f(b) < 0 )
+            {
+                a = x;
+                error = abs( (b-a)/2 );
+            }
+            else
+            {
+                b = x;
+                error = abs( (b-a)/2 );
 
+
+            }
+        } while (error >= 0.000001);
+        cout << x;
+    }
+    else {
+        do {
+            imput(a,b);
+        } while (!control(a, b));
+        bisezione(a,b);
+    }
+}
 bool control(float a, float b){
     if (f(a)*f(b)< 0)
     {
@@ -43,42 +72,9 @@ bool control(float a, float b){
         return false;
     }
 }
-
-void bisezione(float a, float b){
-    float x = 0 ;
-    float error = 0;
-    if (control(a,b))
-    {
-        do{
-            x = (a + b) / 2;
-            if (f(x) == 0)
-            {
-                break;
-            }
-            else if (f(x) * f(b) < 0 )
-            {
-                a = x;
-                error = abs( (b - a) / 2 );
-            }
-            else
-            {
-                b = x;
-                error = abs( (b - a ) / 2 );
-            }
-        } while (error >= 0.000001);
-        if ( x < 0 ) {
-            cout << rid(x) - 0.0001;
-            }
-        else {
-            cout << rid(x);
-        }
-    }
-    else {
-        do {
-            cout << "inserire estremi" << endl;
-            cin >> a;
-            cin >> b;
-        } while (!control(a, b));
-        bisezione(a,b);
-    }
+void imput(float a, float b){
+    cout << "inserire estremi" << endl;
+    cin >> a;
+    cin >> b;
+    return;
 }
